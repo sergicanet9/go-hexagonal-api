8abed3b4c0358aed50651b6c26be422d7dc8372d
Sergi Canet <s.canet9@outlook.com>
2022-12-08 23:36:45 +0100

    composite actions

--- db/postgres/user_test.go
+++ db/postgres/user_test.go
@@ -1,14 +1,18 @@
 package postgres
 
 import (
+	"context"
 	"testing"
 
+	"github.com/sergicanet9/go-hexagonal-api/core/entities"
+	"github.com/sergicanet9/scv-go-tools/v3/infrastructure"
 	"github.com/sergicanet9/scv-go-tools/v3/mocks"
 	"github.com/stretchr/testify/assert"
 )
 
 // TestNewUserRepository_Ok checks that TestNewUserRepository creates a new userRepository struct
 func TestNewUserRepository_Ok(t *testing.T) {
+	// Arrange
 	_, db := mocks.NewSqlDB(t)
 	defer db.Close()
 
@@ -17,5 +21,25 @@ func TestNewUserRepository_Ok(t *testing.T) {
 
 	// Assert
 	assert.NotEmpty(t, repo)
+}
 
+// TODO...
+func TestCreate_Ok(t *testing.T) {
+	// Arrange
+	_, db := mocks.NewSqlDB(t)
+	defer db.Close()
+	repo := &userRepository{
+		infrastructure.PostgresRepository{
+			DB: db,
+		},
+	}
+
+	newUser := entities.User{}
+
+	// Act
+	_, _ = repo.Create(context.Background(), newUser)
+
+	// Assert
+	// assert.IsType(t, newUser.ID, id)
+	// assert.Equal(t, nil, err)
 }
