521d88fd43b1be69d38bd5a12e427045f59737af
Sergi Canet <sergi.canet@derivco.se>
2025-09-05 09:58:12 +0200

    try fix tests

--- app/async/healthchecker/healthchecker.go
+++ app/async/healthchecker/healthchecker.go
@@ -25,31 +25,29 @@ func RunHTTP(ctx context.Context, cancel context.CancelFunc, url string, interva
 	for ctx.Err() == nil {
 		<-time.After(interval)
 
-		func() {
-			req, err := http.NewRequest(http.MethodGet, url, http.NoBody)
-			if err != nil {
-				observability.Logger().Printf("HTTP healthchecker process - error: %s", err)
-				return
-			}
-			req.Header.Set("Content-Type", contentType)
+		req, err := http.NewRequest(http.MethodGet, url, http.NoBody)
+		if err != nil {
+			observability.Logger().Printf("HTTP healthchecker process - error: %s", err)
+			continue
+		}
+		req.Header.Set("Content-Type", contentType)
 
-			start := time.Now()
-			resp, err := http.DefaultClient.Do(req)
-			elapsed := time.Since(start)
+		start := time.Now()
+		resp, err := http.DefaultClient.Do(req)
+		elapsed := time.Since(start)
 
-			if err != nil {
-				observability.Logger().Printf("HTTP healthchecker process - error: %s", err)
-				return
-			}
-			defer resp.Body.Close()
+		if err != nil {
+			observability.Logger().Printf("HTTP healthchecker process - error: %s", err)
+			continue
+		}
+		defer resp.Body.Close()
 
-			if resp.StatusCode != http.StatusOK {
-				observability.Logger().Printf("HTTP healthchecker process - error: %s", err)
-				return
-			}
+		if resp.StatusCode != http.StatusOK {
+			observability.Logger().Printf("HTTP healthchecker process - error: %s", err)
+			continue
+		}
 
-			observability.Logger().Printf("HTTP healthchecker process - health Check complete, time elapsed: %s", elapsed)
-		}()
+		observability.Logger().Printf("HTTP healthchecker process - health Check complete, time elapsed: %s", elapsed)
 	}
 }
 
@@ -64,29 +62,27 @@ func RunGRPC(ctx context.Context, cancel context.CancelFunc, target string, inte
 	for ctx.Err() == nil {
 		<-time.After(interval)
 
-		func() {
-			conn, err := grpc.NewClient(target, grpc.WithTransportCredentials(insecure.NewCredentials()))
-			if err != nil {
-				observability.Logger().Printf("gRPC healthchecker process - error: %v", err)
-				return
-			}
-			defer conn.Close()
-			client := pb.NewHealthServiceClient(conn)
+		conn, err := grpc.NewClient(target, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			observability.Logger().Printf("gRPC healthchecker process - error: %v", err)
+			continue
+		}
+		defer conn.Close()
+		client := pb.NewHealthServiceClient(conn)
 
-			start := time.Now()
-			_, err = client.HealthCheck(ctx, nil)
-			elapsed := time.Since(start)
+		start := time.Now()
+		_, err = client.HealthCheck(ctx, nil)
+		elapsed := time.Since(start)
 
-			if err != nil {
-				observability.Logger().Printf("gRPC healthchecker process - error: %v", err)
-				return
-			}
+		if err != nil {
+			observability.Logger().Printf("gRPC healthchecker process - error: %v", err)
+			continue
+		}
 
-			if _, ok := status.FromError(err); !ok {
-				observability.Logger().Printf("gRPC healthchecker process - error: %v", err)
-			}
+		if _, ok := status.FromError(err); !ok {
+			observability.Logger().Printf("gRPC healthchecker process - error: %v", err)
+		}
 
-			observability.Logger().Printf("gRPC healthchecker process - health Check complete, time elapsed: %s", elapsed)
-		}()
+		observability.Logger().Printf("gRPC healthchecker process - health Check complete, time elapsed: %s", elapsed)
 	}
 }
